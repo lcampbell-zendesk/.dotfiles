817a4e2150ece92b2ca523c85248a09bbee761b5
Daniel Bogan <d@waferbaby.com>
2014-06-16 18:05:00 -0700

    Initial import.

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+ln -sf ~/.dotfiles/zsh/config ~/.zshrc
+ln -sf ~/.dotfiles/vim/config ~/.vimrc
+
+ln -sf ~/.dotfiles/git/config ~/.gitconfig
+ln -sf ~/.dotfiles/git/ignores ~/.gitignore_global
+
+ln -sf ~/.dotfiles/sublime ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User
