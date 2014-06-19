a03b76543a46b5efc01c5002ed103fd40ac94644
Daniel Bogan <d@waferbaby.com>
2014-06-19 16:50:57 -0700

    Use the right path for the git ignores.

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ ln -sf ~/.dotfiles/zsh/config ~/.zshrc
 ln -sf ~/.dotfiles/vim/config ~/.vimrc
 
 ln -sf ~/.dotfiles/git/config ~/.gitconfig
-ln -sf ~/.dotfiles/git/ignores ~/.gitignore_global
+ln -sf ~/.dotfiles/git/global_ignores ~/.gitignore_global
 
 ln -sf ~/.dotfiles/sublime ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User
