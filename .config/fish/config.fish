527ebc8cd95b24197be024f6d4e5e8713209c06b
Daniel Bogan <d@waferbaby.com>
2014-08-23 23:26:28 -0700

    Removed the universal option from the paths, split the prompt function into a separate file.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,16 +1,3 @@
-set --universal fish_user_paths $fish_user_paths /usr/local/bin/
-
+set fish_user_paths $fish_user_paths /usr/local/bin/
 set fish_greeting
-
 set __fish_git_prompt_color_branch yellow
-
-function fish_prompt
-  set last_status $status
-  
-  set_color $fish_color_host
-  printf '⚡ ️ %s@%s ' (whoami) (hostname|cut -d . -f 1)
-  set_color $fish_color_cwd
-  printf '%s' (prompt_pwd)
-  set_color normal
-  printf '%s 👉  ' (__fish_git_prompt)
-end
