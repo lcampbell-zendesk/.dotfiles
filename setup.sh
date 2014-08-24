5f0847db08e7f4a949609f510bd58e672e304b31
Daniel Bogan <d@waferbaby.com>
2014-08-23 23:19:18 -0700

    Changed the vim path, added the .config directory.

--- setup.sh
+++ setup.sh
@@ -2,7 +2,9 @@
 
 ln -sf ~/.dotfiles/.zshrc ~/
 ln -sf ~/.dotfiles/.vimrc ~/
-ln -sf ~/.dotfiles/vim ~/.vim
+ln -sf ~/.dotfiles/.vim ~/
+
+ln -sf ~/.dotfiles/.config ~/
 
 ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/.dotfiles/.gitignore_global ~/
