0797277f79ac31704f885553e5ed457504da95d2
Daniel Bogan <d@waferbaby.com>
2014-08-24 04:59:17 -0700

    Added explicit user and host colours.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,6 @@
 set fish_user_paths $fish_user_paths /usr/local/bin/
 set fish_greeting
 set __fish_git_prompt_color_branch yellow
+
+set fish_color_user 0000ff
+set fish_color_host 0000ff\x1e\x2d\x2dunderline
