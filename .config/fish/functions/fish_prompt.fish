f9e94429af970c991916e6fb3acca6a09a805c89
Daniel Bogan <d@waferbaby.com>
2014-08-23 23:37:33 -0700

    Updated to use the user colour.

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,10 +1,13 @@
 function fish_prompt
   set last_status $status
   
+  printf '⚡ ️ '
+  set_color $fish_color_user
+  printf '%s@' (whoami)
   set_color $fish_color_host
-  printf '⚡ ️ %s@%s ' (whoami) (hostname|cut -d . -f 1)
-  set_color $fish_color_cwd
-  printf '%s' (prompt_pwd)
+  printf '%s' (hostname|cut -d . -f 1)
   set_color normal
+  set_color $fish_color_cwd
+  printf ' %s' (prompt_pwd)
   printf '%s 👉  ' (__fish_git_prompt)
 end
