ff79de983545870238eae3dde1bf1ee1701b15ef
Daniel Bogan <d@waferbaby.com>
2014-08-24 05:15:00 -0700

    Added more explicit colours. Removed the underline from the host.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,5 +2,8 @@ set fish_user_paths $fish_user_paths /usr/local/bin/
 set fish_greeting
 set __fish_git_prompt_color_branch yellow
 
+set fish_color_cwd aaaaaa
+set fish_color_command ffffff
+set fish_color_param ffffff
 set fish_color_user 0000ff
-set fish_color_host 0000ff\x1e\x2d\x2dunderline
+set fish_color_host 0000ff
