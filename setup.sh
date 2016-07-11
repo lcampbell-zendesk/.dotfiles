c2611c52d349fc4de23eb282958f5db2512f0e60
Daniel Bogan <d@waferbaby.com>
2016-07-11 16:43:42 -0700

    Added comments, the font cask and my favourite shell font.

--- setup.sh
+++ setup.sh
@@ -1,20 +1,30 @@
 #!/usr/bin/env sh
 
+# Symlink all the things.
+
 ln -sf ~/.dotfiles/.zshrc ~/
 ln -sf ~/.dotfiles/.vimrc ~/
 ln -sf ~/.dotfiles/.vim ~/
-
 ln -sf ~/.dotfiles/.config ~/
-
 ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/.dotfiles/.gitignore_global ~/
-
 ln -sf ~/.dotfiles/.atom ~/
+
+# Install homebrew.
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# Install fonts.
+
+brew tap caskroom/fonts
+brew cask install font-mononoki
+
+# Install Mac apps.
+
 brew cask install atom
 brew cask install iterm2
+
+# Make typing not suck.
 
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 defaults write NSGlobalDomain KeyRepeat -int 0
