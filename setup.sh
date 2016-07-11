6062271d1597f8aaff8c5623813ad3829b7ef0fe
Daniel Bogan <d@waferbaby.com>
2016-07-11 14:59:52 -0700

    Added the brew install, and used cask to install Atom and iTerm2.

--- setup.sh
+++ setup.sh
@@ -10,3 +10,8 @@ ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/.dotfiles/.gitignore_global ~/
 
 ln -sf ~/.dotfiles/.atom ~/
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew cask install atom
+brew cask install iterm2
