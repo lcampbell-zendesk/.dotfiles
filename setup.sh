588337c51877a73fbd9f0d15e63e075e98f76c9c
Daniel Bogan <d@waferbaby.com>
2016-07-21 13:19:35 -0700

    Gave up on Atom, switched back to Sublime.

--- setup.sh
+++ setup.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env sh
 
+# Prepare the directories.
+
+mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages
+
 # Symlink all the things.
 
-ln -sf ~/.dotfiles/.zshrc ~/
-ln -sf ~/.dotfiles/.vimrc ~/
-ln -sf ~/.dotfiles/.vim ~/
 ln -sf ~/.dotfiles/.config ~/
 ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/.dotfiles/.gitignore_global ~/
-ln -sf ~/.dotfiles/.atom ~/
+ln -sf ~/.dotfiles/.sublime ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+ln -sf ~/.dotfiles/.vimrc ~/
+ln -sf ~/.dotfiles/.vim ~/
+ln -sf ~/.dotfiles/.zshrc ~/
 
 # Install homebrew.
 
@@ -22,12 +26,12 @@ brew cask install font-mononoki
 # Install Mac apps.
 
 brew cask install appcleaner
-brew cask install atom
 brew cask install iterm2
 brew cask install launchbar
 brew cask install monolingual
 brew cask install moom
 brew cask install slack
+brew cask install sublime-text
 
 # Make typing not suck.
 
