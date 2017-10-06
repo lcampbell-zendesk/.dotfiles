a277f513996ce094b07a89e19224e3be0db9e677
Daniel Bogan <d@waferbaby.com>
2017-10-06 18:29:49 +1100

    Move the rbenv shims earlier in our path.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
 set fish_greeting
 
-set -g fish_user_paths "/usr/local/bin" "/usr/local/sbin" "$HOME/.rbenv/shims" $fish_user_paths
+set -g fish_user_paths "$HOME/.rbenv/shims" "/usr/local/bin" "/usr/local/sbin" $fish_user_paths
 set -x EDITOR /usr/local/bin/subl
 
 rbenv rehash >/dev/null ^&1
